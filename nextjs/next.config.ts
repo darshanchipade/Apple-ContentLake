5fb9e3ae1783ac2942b977593ffa2f601eba17f1
darshanchipade <dchipade@gmail.com>
2026-02-16 13:23:30 -0800

    Updated the config

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ea854xr24n6.exactdn.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
